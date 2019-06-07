815b1fc3302427c8c1b835d4d84715ed5946a97c
Vaishak Murali <33431826+vaishakmurali1997@users.noreply.github.com>
2019-06-07 13:41:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,218 +1,302 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import './index.css';
-import ic_back from './ic_back.svg';
-import ic_forward from './ic_forward.svg';
+'use strict';
 
-const config = {
-    months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    month_subs: ['Jan', 'Feb', 'Apr', 'Mar', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
-    weeks: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-    week_subs: ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'],
-    today: function() {
-      return new Date();
-    }
-}
-const TODAY = config.today();
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-class Calendar extends Component {
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-  constructor(props) {
-    super(props);
-    this.state = {
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+require('./index.css');
+
+var _ic_back = require('./ic_back.svg');
+
+var _ic_back2 = _interopRequireDefault(_ic_back);
+
+var _ic_forward = require('./ic_forward.svg');
+
+var _ic_forward2 = _interopRequireDefault(_ic_forward);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var config = {
+  months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+  month_subs: ['Jan', 'Feb', 'Apr', 'Mar', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+  weeks: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+  week_subs: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+  today: function today() {
+    return new Date();
+  }
+};
+var TODAY = config.today();
+
+var Calendar = function (_Component) {
+  _inherits(Calendar, _Component);
+
+  function Calendar(props) {
+    _classCallCheck(this, Calendar);
+
+    var _this = _possibleConstructorReturn(this, (Calendar.__proto__ || Object.getPrototypeOf(Calendar)).call(this, props));
+
+    _this.state = {
       current: config.today(),
       selected: config.today(),
       ldom: 30
     };
+    return _this;
   }
 
-  componentWillMount() {
-    this.updateMonth(0);
-  }
-
-  updateMonth(add) {
-    var d = this.state.current;
-    d.setMonth(d.getMonth() + add);
-    var eom = new Date(d.getYear(), d.getMonth() + 1, 0).getDate();
-    this.setState({
-      current: d,
-      ldom: eom
-    });
-  }
-
-  prev() {
-    this.updateMonth(-1);
-  }
-
-  next() {
-    this.updateMonth(1);
-  }
-
-  _onDatePicked(month, day) {
-    var d = new Date(this.state.current.getTime());
-    d.setMonth(d.getMonth() + month);
-    d.setDate(day);
-    this.props.onDatePicked(d);
-    this.setState({
-      selected: d
-    });
-  }
-
-  renderDay(opts={}) {
-    var baseClasses = "day noselect";
-    var today = "";
-    var todayStyle = {};
-    var containerStyle = {};
-    if( opts.today ) {
-      today = "current";
-      todayStyle = {
-        borderColor: this.props.accentColor,
-      };
+  _createClass(Calendar, [{
+    key: 'componentWillMount',
+    value: function componentWillMount() {
+      this.updateMonth(0);
     }
-
-    var selected = "";
-    var selectedStyle = {};
-    if( opts.selected ) {
-      selected = "selected";
-      selectedStyle = {
-        backgroundColor: this.props.accentColor
-      }
-      containerStyle = {
-        color: '#ffffff'
-      }
+  }, {
+    key: 'updateMonth',
+    value: function updateMonth(add) {
+      var d = this.state.current;
+      d.setMonth(d.getMonth() + add);
+      var eom = new Date(d.getYear(), d.getMonth() + 1, 0).getDate();
+      this.setState({
+        current: d,
+        ldom: eom
+      });
     }
+  }, {
+    key: 'prev',
+    value: function prev() {
+      this.updateMonth(-1);
+    }
+  }, {
+    key: 'next',
+    value: function next() {
+      this.updateMonth(1);
+    }
+  }, {
+    key: '_onDatePicked',
+    value: function _onDatePicked(month, day) {
+      var d = new Date(this.state.current.getTime());
+      d.setMonth(d.getMonth() + month);
+      d.setDate(day);
+      this.props.onDatePicked(d);
+      this.setState({
+        selected: d
+      });
+    }
+  }, {
+    key: 'renderDay',
+    value: function renderDay() {
+      var _this2 = this;
 
-    baseClasses += opts.current ? "" : " non-current";
+      var opts = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
-    return (<div className={baseClasses}
-                style={containerStyle}>
-              <div className={today} style={todayStyle}></div>
-              <div className={selected} style={selectedStyle}></div>
-              <p onClick={ (ev) => {
-                var day = ev.target.innerHTML;
-                this._onDatePicked(opts.month, day);
-              }}>{opts.date.getDate()}</p>
-            </div>);
-  }
-
-  renderDays(copy) {
-    var days = [];
-
-    // set to beginning of month
-    copy.setDate(1);
-
-    // if we are missing no offset, include the previous week
-    var offset = copy.getDay() === 0 ? 7 : copy.getDay();
-
-    copy.setDate(-offset);
-
-    var inMonth = false;
-    var lastMonth = true;
-    for (var i = 0; i < 42; i++) {
-      // increase date
-      copy.setDate(copy.getDate() + 1);
-
-      // make sure we pass any previous month values
-      if (i < 30 && copy.getDate() === 1) {
-        inMonth = true;
-        lastMonth = false;
-      }
-      // if we are seeing the '1' again, we have iterated over
-      // the current month
-      else if (i > 30 && copy.getDate() === 1) {
-        inMonth = false;
+      var baseClasses = "calender-day calender-noselect2";
+      var today = "";
+      var todayStyle = {};
+      var containerStyle = {};
+      if (opts.today) {
+        today = "calender-current";
+        todayStyle = {
+          borderColor: this.props.accentColor
+        };
       }
 
-      var sel = new Date(this.state.selected.getTime());
-      var isSelected = (sel.getFullYear() === copy.getFullYear() &&
-          sel.getDate() === copy.getDate() &&
-          sel.getMonth() === copy.getMonth());
+      var selected = "";
+      var selectedStyle = {};
+      if (opts.selected) {
+        selected = "calender-selected";
+        selectedStyle = {
+          backgroundColor: this.props.accentColor,
+          color: this.props.accentColor
+        };
+        containerStyle = {
+          color: "#0091FF"
+        };
+      }
 
-      var isToday = (TODAY.getFullYear() === copy.getFullYear() &&
-          TODAY.getDate() === copy.getDate() &&
-          TODAY.getMonth() === copy.getMonth());
+      baseClasses += opts.current ? "" : " calender-non-current";
 
-      days.push(this.renderDay({
-        today: isToday,
-        selected: isSelected,
-        current: inMonth,
-        month: (inMonth ? 0 : (lastMonth ? -1 : 1)),
-        date: copy
-      }));
+      return _react2.default.createElement(
+        'div',
+        { className: baseClasses,
+          style: containerStyle },
+        _react2.default.createElement('div', { className: today, style: todayStyle }),
+        _react2.default.createElement('div', { className: selected, style: selectedStyle }),
+        _react2.default.createElement(
+          'h5',
+
+          { className: 'calender-h5',
+            onClick: function onClick(ev) {
+              var day = ev.target.innerHTML;
+              _this2._onDatePicked(opts.month, day);
+            } },
+          opts.date.getDate()
+        )
+      );
     }
+  }, {
+    key: 'renderDays',
+    value: function renderDays(copy) {
+      var days = [];
 
-    return days;
-  }
+      // set to beginning of month
+      copy.setDate(1);
 
-  renderHeaders() {
-    var header = [];
+      // if we are missing no offset, include the previous week
+      var offset = copy.getDay() === 0 ? 7 : copy.getDay();
 
-    for (var i = 0; i < config.week_subs.length; i++) {
-      header.push(<p className='day-headers noselect'>
-                    {config.week_subs[i]}
-                  </p>);
+      copy.setDate(-offset);
+
+      var inMonth = false;
+      var lastMonth = true;
+      for (var i = 0; i < 42; i++) {
+        // increase date
+        copy.setDate(copy.getDate() + 1);
+
+        // make sure we pass any previous month values
+        if (i < 30 && copy.getDate() === 1) {
+          inMonth = true;
+          lastMonth = false;
+        }
+        // if we are seeing the '1' again, we have iterated over
+        // the current month
+        else if (i > 30 && copy.getDate() === 1) {
+            inMonth = false;
+          }
+
+        var sel = new Date(this.state.selected.getTime());
+        var isSelected = sel.getFullYear() === copy.getFullYear() && sel.getDate() === copy.getDate() && sel.getMonth() === copy.getMonth();
+
+        var isToday = TODAY.getFullYear() === copy.getFullYear() && TODAY.getDate() === copy.getDate() && TODAY.getMonth() === copy.getMonth();
+
+        days.push(this.renderDay({
+          today: isToday,
+          selected: isSelected,
+          current: inMonth,
+          month: inMonth ? 0 : lastMonth ? -1 : 1,
+          date: copy
+        }));
+      }
+
+      return days;
     }
+  }, {
+    key: 'renderHeaders',
+    value: function renderHeaders() {
+      var header = [];
 
-    return header;
-  }
+      for (var i = 0; i < config.week_subs.length; i++) {
+        header.push(_react2.default.createElement(
+          'p',
+          { className: 'calender-day-headers calender-noselect' },
+          config.week_subs[i]
+        ));
+      }
 
-  render() {
-    // get su-sat header
-    var header = this.renderHeaders();
-
-    // copy our current time state
-    var copy = new Date(this.state.current.getTime());
-
-    // get the month days
-    var days = this.renderDays(copy);
-
-    var tMonth = config.months[this.state.selected.getMonth()];
-    var tDate = this.state.selected.getDate();
-    var month = config.months[this.state.current.getMonth()];
-    var year = this.state.current.getFullYear();
-    var date = this.state.current.getDate();
-
-    var upperDate = null;
-    if( this.props.showHeader ) {
-      upperDate = (<div className='flex-2 header center' style={{
-          backgroundColor: this.props.accentColor
-        }}>
-        <p className="header-month">{tMonth.toUpperCase()}</p>
-        <p className="header-day">{tDate}</p>
-      </div>);
+      return header;
     }
-    return (<div className={this.props.orientation}>
-      {upperDate}
-      <div className="padding">
-        <div className='month'>
-          <img className="month-arrow-left" src={ic_back} alt="back" onClick={this.prev.bind(this)}></img>
-          <p className="month-title">{month}<br/>
-          <span className="month-year">{year}</span>
-          </p>
-          <img className="month-arrow-right" src={ic_forward} alt="forward" onClick={this.next.bind(this)}></img>
-        </div>
-        <div className='footer'>
-          {header}
-          {days}
-        </div>
-      </div>
-    </div>);
-  }
+  }, {
+    key: 'render',
+    value: function render() {
+      // get su-sat header
+      var header = this.renderHeaders();
 
-};
+      // copy our current time state
+      var copy = new Date(this.state.current.getTime());
+
+      // get the month days
+      var days = this.renderDays(copy);
+
+      var tMonth = config.months[this.state.selected.getMonth()];
+      var tDate = this.state.selected.getDate();
+      var month = config.months[this.state.current.getMonth()];
+      var year = this.state.current.getFullYear();
+      var date = this.state.current.getDate();
+
+      var upperDate = null;
+      if (this.props.showHeader) {
+        upperDate = _react2.default.createElement(
+          'div',
+          { className: 'flex-2 calender-header center', style: {
+              backgroundColor: this.props.accentColor
+            } },
+          _react2.default.createElement(
+            'p',
+            { className: 'calender-header-month' },
+            tMonth.toUpperCase()
+          ),
+          _react2.default.createElement(
+            'p',
+            { className: 'calender-header-day' },
+            tDate
+          )
+        );
+      }
+      return _react2.default.createElement(
+        'div',
+        { className: this.props.orientation },
+        upperDate,
+        _react2.default.createElement(
+          'div',
+          { className: 'calender-padding' },
+          _react2.default.createElement(
+            'div',
+            { className: 'calender-month' },
+            _react2.default.createElement('img', { className: 'calender-month-arrow-left', src: _ic_back2.default, alt: 'back', onClick: this.prev.bind(this) }),
+            _react2.default.createElement(
+              'p',
+              { className: 'calender-month-title' },
+              month,
+              _react2.default.createElement('br', null),
+              _react2.default.createElement(
+                'span',
+                { className: 'calender-month-year' },
+                year
+              )
+            ),
+            _react2.default.createElement('img', { className: 'calender-month-arrow-right', src: _ic_forward2.default, alt: 'forward', onClick: this.next.bind(this) })
+          ),
+          _react2.default.createElement(
+            'div',
+            { className: 'calender-footer' },
+            header,
+            days
+          )
+        )
+      );
+    }
+  }]);
+
+  return Calendar;
+}(_react.Component);
+
+;
 
 Calendar.propTypes = {
-  accentColor: PropTypes.string,
-  onDatePicked: PropTypes.func,
-  showHeader: PropTypes.bool,
-  orientation: PropTypes.string,
+  accentColor: _propTypes2.default.string,
+  onDatePicked: _propTypes2.default.func,
+  showHeader: _propTypes2.default.bool,
+  orientation: _propTypes2.default.string
 };
 
 Calendar.defaultProps = {
-  accentColor: '#00C1A6',
-  onDatePicked: function(){},
+  accentColor: '#FFF',
+  onDatePicked: function onDatePicked() {},
   showHeader: true,
   orientation: 'flex-col'
 };
 
-export default Calendar;
+exports.default = Calendar;
